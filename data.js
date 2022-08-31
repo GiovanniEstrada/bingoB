cbc4bb98fb04d06c50a6fd43928a2b1fadb01858
GiovanniEstrada <2991897830101@ingenieria.usac.edu.gt>
2022-08-30 18:26:21 -0600

    All task uploaded

--- data.js
+++ data.js
@@ -1,21 +1,108 @@
 const basicos = [
   {
-    id: 1,
-    descripcion: "Basicos",
+    id: 6,
+    descripcion:
+      "Resolver ejercicio de conteo y sumatoria. Hoja 1 (Cantidad por cargo y suma de sueldos)",
   },
   {
     id: 2,
-    descripcion: "",
+    descripcion: "Responda la pregunta 5 de la hoja 5 ",
   },
-];
-
-const primaria = [
+  {
+    id: 3,
+    descripcion:
+      "Resolver ejercicio de conteo y sumatoria. Hoja 2 (Cantidad de cada distrito y donaciones)",
+  },
+  {
+    id: 4,
+    descripcion: "Resolver ejercicio de cuenta de ahorro en Banco. Hoja 3 ",
+  },
+  {
+    id: 5,
+    descripcion: "Resolver ejercicio 1 de ventas por meses y stand. Hoja 4 ",
+  },
   {
     id: 1,
-    descripcion: "Primaria",
+    descripcion: "Responda la pregunta 1 de la hoja 5",
   },
   {
-    id: 2,
-    descripcion: "",
+    id: 7,
+    descripcion: "Responda la pregunta 2 que se encuentra en la hoja 5",
+  },
+  {
+    id: 8,
+    descripcion:
+      "Realizar el ejercicio 2 de Masculino y Femenino en la hoja 3 ",
+  },
+  {
+    id: 9,
+    descripcion: "Responda la pregunta 3 de la hoja 5 ",
+  },
+  {
+    id: 10,
+    descripcion: "Resolver ejercicio 2 de formulas de la hoja 1",
+  },
+  {
+    id: 11,
+    descripcion: "Resolver el ejercicio 2 de concatenar de la hoja 4 ",
+  },
+  {
+    id: 12,
+    descripcion: "Resuelva el ejercicio de concatenar y filtros de la hoja 6",
+  },
+  {
+    id: 13,
+    descripcion: "Responda la pregunta 4 de la hoja 5 ",
+  },
+  {
+    id: 14,
+    descripcion: "Responda la pregunta 6 de la hoja 5",
+  },
+  {
+    id: 15,
+    descripcion: "Resuelva el ejercicio 2 sobre notas de alumnos de la hoja 2 ",
+  },
+  {
+    id: 16,
+    descripcion: "Responda la pregunta 8 de la hoja 5 ",
+  },
+  {
+    id: 17,
+    descripcion: "Responda la pregunta 7 de la hoja 5 ",
+  },
+  {
+    id: 18,
+    descripcion: "Responda la pregunta 9 de la hoja 5 ",
+  },
+  {
+    id: 19,
+    descripcion:
+      "Resolver ejercicio de conteo y sumatoria. Hoja 1 (Cantidad por cargo y suma de sueldos)",
+  },
+  {
+    id: 20,
+    descripcion: "Resolver ejercicio 1 de ventas por meses y stand. Hoja 4 ",
+  },
+  {
+    id: 21,
+    descripcion: "Responda la pregunta 10 de la hoja 5 ",
+  },
+  {
+    id: 22,
+    descripcion: "Responda la pregunta 3 de la hoja 5",
+  },
+  {
+    id: 23,
+    descripcion:
+      "Realizar el ejercicio 2 de Masculino y Femenino en la hoja 3 ",
+  },
+  {
+    id: 24,
+    descripcion:
+      "Resolver ejercicio de conteo y sumatoria. Hoja 2 (Cantidad de cada distrito y donaciones)",
+  },
+  {
+    id: 25,
+    descripcion: "Responda la pregunta 11 de la hoja 5 ",
   },
 ];
